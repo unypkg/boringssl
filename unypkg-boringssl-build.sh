f186d5896c39be7ba3d0a7627a422904dc13b873
michacassola <mich@casso.la>
2024-06-19 18:01:02 +0200

    Update unypkg-boringssl-build.sh

--- unypkg-boringssl-build.sh
+++ unypkg-boringssl-build.sh
@@ -35,7 +35,7 @@ mkdir -pv /uny/sources
 cd /uny/sources || exit
 
 pkgname="boringssl"
-pkggit="https://boringssl.googlesource.com/boringssl refs/heads/master"
+pkggit="https://boringssl.googlesource.com/boringssl"
 #gitdepth="--depth=1"
 
 ### Get version info from git remote
